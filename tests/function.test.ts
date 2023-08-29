fd47a93cf9319786cc13df6b4ac5fac943f04f85
katsumiok <50365735+katsumiok@users.noreply.github.com>
2023-08-29 17:13:39 -0400

    Fix a test case

--- tests/function.test.ts
+++ tests/function.test.ts
@@ -4,7 +4,7 @@ import * as t from '../src/types';
 describe('Define', () => {
   it('xxx', async () => {
     const f = define(t.number, 'Find the third prime number.');
-    const number = await f.call({});
+    const number = await f({});
     expect(number).toEqual(5);
   });
 });
